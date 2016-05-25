c8ec8e13760936a92a7470cc8b50a0c799b15a09
qweraqq <shenxiangxiang@gmail.com>
2016-05-25 08:49:04 +0800

    first commit

--- start_fio.py
+++ start_fio.py
@@ -3,11 +3,15 @@
 import sys
 import os
 import getopt
-import thread
+from threading import Thread
 import libvirt
 import socket
 import paramiko
 
+user_name = 'root'
+password = 'qwer1011'
+fio_command1 = 'fio1'
+fio_command2 = 'fio2'
 
 def getIPAddresses():
     """
@@ -21,11 +25,30 @@ def getIPAddresses():
     for lease in conn.networkLookupByName("my_network").DHCPLeases():
         print(lease)
     """
-    pass
+    conn = libvirt.open('qemu+ssh://root@localhost/system')
+    ip_lists = conn.networkLookupByName("my_network").DHCPLeases()
+    return ip_lists
 
 
 def startFio(fio_num):
-    pass
+    ip_lists = getIPAddresses()
+    if len(ip_lists) < fio_num:
+        print "not enough hosts"
+        sys.exit(2)
+    for idx, ip in enumerate(ip_lists):
+        if idx > fio_num:
+            break
+        t = Thread(target=fioThread, args=(ip,))
+        t.start()
+
+
+def fioThread(ip):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(ip, 22, user_name, password)
+    stdin, stdout, stderr = ssh.exec_command(fio_command1)
+    stdin, stdout, stderr = ssh.exec_command(fio_command2)
+    ssh.close()
 
 
 def main(argv):
