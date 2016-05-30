212bf17a770a5b7ec40b2239c1ae354bb7929d2f
qweraqq <shenxiangxiang@gmail.com>
2016-05-30 13:59:28 +0800

    add prepate test

--- prepare_test.py
+++ prepare_test.py
@@ -11,6 +11,8 @@ import paramiko
 user_name = 'root'
 password = 'qwer1011'
 host_name = socket.gethostname()
+dd_command = 'dd if=/dev/zero of=/dev/vdb bs=1M count=30k'
+
 
 def getIPAddresses():
     """
@@ -47,7 +49,36 @@ def cleanUp(ips):
         ssh.close()
 
 
-def main():
+def startDD(ip_lists):
+    for idx, ip in enumerate(ip_lists):
+        t = Thread(target=ddThread, args=(ip, ))
+        t.start()
+
+
+def ddThread(ip):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(ip, 22, user_name, password)
+    stdin, stdout, stderr = ssh.exec_command(dd_command)
+    print stderr.read()
+    print stdout.read()
+    ssh.close()
+
+
+def main(argv):
+    dd_sig = 0
+    try:
+        opts, args = getopt.getopt(argv, "hn:w:")
+    except getopt.GetoptError:
+        print 'usage:prepare_test -w <dd_signal>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-w':
+            dd_sig = int(arg)
+        if opt == '-h':
+            print 'usage:prepare_test -w <dd_signal>'
+            sys.exit()
+    # basic function
     ips = getIPAddresses()
     with open(host_name+'_vm_ips.py', 'w') as f:
         f.write("""#!/usr/bin/python
@@ -58,6 +89,10 @@ def main():
             f.write("""\"{0}\", """.format(ip))
         f.write("""]""")
     cleanUp(ips)
+    # dd
+    if dd_sig > 0:
+        startDD(ips)
+
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
